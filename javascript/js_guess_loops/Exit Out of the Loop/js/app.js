720620f939eb400b49d362986e48b7472f3bc447
Matto <matt.mcquain@gmail.com>
2020-07-31 23:53:51 -0700

    added player guessing hint conditional statement and finialized guessing game

--- javascript/js_guess_loops/Exit Out of the Loop/js/app.js
+++ javascript/js_guess_loops/Exit Out of the Loop/js/app.js
@@ -17,3 +17,26 @@ for ( let i = 1; i <= 10; i++ ) {
 }
 
 main.innerHTML = `<h1>${message}</h1>`;
+
+// Conditional statement to give players hints about the number to guess
+
+// const main = document.querySelector('main');
+// const randomNumber = getRandomNumber(10);
+// let message;
+// let guess = prompt(`Guess a number between 1 and 10.`);
+
+// function getRandomNumber(upper) {...}
+
+// for ( let i = 9; i >= 1; i-- ) {
+//   if ( +guess > randomNumber ) {
+//     guess = prompt(`Guess a lower number. You have ${i} more tries.`);
+//   } else if ( +guess < randomNumber) {
+//     guess = prompt(`Guess a higher number. You have ${i} more tries.`);
+//   } else if ( +guess === randomNumber ) {
+//     message = `You guessed the number! It was ${randomNumber}.`;
+//     break;
+//   }
+//   message = `You did not guess the number. It was ${randomNumber}.`;
+// }
+
+// main.innerHTML = `<h1>${message}</h1>`;

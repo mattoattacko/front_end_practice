1d50155afceedfc4dee4d8e351270fe556241cb8
Matto <matt.mcquain@gmail.com>
2020-08-02 22:50:53 -0700

    debugged and completed product array

--- javascript/js_object_literals_products_cli/final_products.js
+++ javascript/js_object_literals_products_cli/final_products.js
@@ -1,14 +1,5 @@
-/* INSTRUCTIONS
-To run this file, click in the Console below and type: node products.js 
-If the console isn't visible, click the View menu above and choose Show Console.
-
-You can clear the console by typing `clear` and pressing enter.
-
-If your program is stuck in an infinite loop, you can break out of the program by typing ctrl + C.
-*/
-
 // 1. Create an array named products.
-// 2. Add objects to the array. Each object should be a single product, with the following 4 properties. 
+// 2. Add objects to the array. Each object should be a single product, with the following 3 properties. 
 // Make up the values for the properties, just make sure the inventory is a whole number (it's the number of that product currently in stock) and unit_price is a floating point number like 45.99
 // -- name
 // -- inventory
